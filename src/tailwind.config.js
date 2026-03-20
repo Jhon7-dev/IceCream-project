4feafbc1b1dd88e5f59e539572a6407a0222cdfd
João Victor Gomes de Souza <gomesjoao2003@gmail.com>
2026-03-20 15:19:46 -0300

    PRIMEIRO COMMIT

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'neutral-primary': '#ffffff',
+        'neutral-secondary-soft': '#f5f5f5',
+        'neutral-tertiary': '#e5e5e5',
+        'heading': '#111827',
+        'body': '#6b7280',
+        'brand': '#ec4899',
+        'fg-brand': '#ec4899',
+        'default': '#e5e7eb',
+      }
+    }
+  }
+}
